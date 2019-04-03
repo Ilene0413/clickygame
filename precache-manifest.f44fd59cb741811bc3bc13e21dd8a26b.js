be7a9f13589e96ab2d2b079ea5a5a252183f5a1b
Ilene Cohen <ilene413@icloud.com>
2019-04-03 11:44:07 -0400

    Updates

---
+++ precache-manifest.f44fd59cb741811bc3bc13e21dd8a26b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "c46d832fca849c235dc0",
+    "url": "/clickygame/static/js/main.731477b3.chunk.js"
+  },
+  {
+    "revision": "debe717764065d6b0037",
+    "url": "/clickygame/static/js/2.84dd4b69.chunk.js"
+  },
+  {
+    "revision": "c46d832fca849c235dc0",
+    "url": "/clickygame/static/css/main.849c76e8.chunk.css"
+  },
+  {
+    "revision": "97db4eb6f9509ce386acbd5e271a6d45",
+    "url": "/clickygame/index.html"
+  }
+];
